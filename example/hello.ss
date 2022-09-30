4ac48d6a3a52b91fca45d590c664f64d95caf1f6
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2022-10-01 02:43:46 +0800

    setup example
    Signed-off-by: Lîm Tsú-thuàn <dannypsnl@gmail.com>

---
+++ example/hello.ss
@@ -0,0 +1,5 @@
+(module hello
+  (export hello))
+
+(: hello : I64 -> I64)
+(fun (hello a) a)
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,24 @@
+use std::fs;
+
 mod parser;
 #[macro_use]
 extern crate lalrpop_util;
+lalrpop_mod!(pub violet);
 
-fn main() {
-    lalrpop_mod!(pub violet);
-
-    #[test]
-    fn calculator1() {
-        assert!(violet::TermParser::new().parse("22").is_ok());
+fn main() -> std::io::Result<()> {
+    let s = fs::read_to_string("example/hello.ss")?;
+    match violet::TermParser::new().parse(s.as_str()) {
+        Ok(_) => {
+            println!("parse ok");
+        }
+        Err(e) => {
+            println!("parse failed {:?}", e);
+        }
     }
+    return Ok(());
+}
+
+#[test]
+fn calculator1() {
+    assert!(violet::TermParser::new().parse("22").is_ok());
 }
