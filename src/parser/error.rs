378280453e21c0c0cf0cf80a9537783bf5abb912
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2022-10-01 22:36:59 +0800

    at least some error fancy
    Signed-off-by: Lîm Tsú-thuàn <dannypsnl@gmail.com>

--- src/parser/error.rs
+++ src/parser/error.rs
@@ -1,1 +1,26 @@
+use miette::{Diagnostic, NamedSource, SourceSpan};
+use thiserror::Error;
 
+#[derive(Diagnostic, Error, Debug)]
+
+pub enum Err {
+    #[error(transparent)]
+    IO(#[from] std::io::Error),
+
+    #[error(transparent)]
+    P(#[from] ParseError),
+}
+
+#[derive(Diagnostic, Error, Debug)]
+#[error("parse failed!")]
+#[diagnostic(code(violet::parse), url(docsrs))]
+pub struct ParseError {
+    #[source_code]
+    pub src: NamedSource,
+
+    #[label("location")]
+    pub bad_bit: SourceSpan,
+
+    #[help("reason")]
+    pub reason: String,
+}
--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -1,27 +1,49 @@
+use lalrpop_util::ParseError::*;
+use miette::{NamedSource, Result};
 use std::fs;
 
 pub mod error;
 pub mod violet;
+use error::{Err, ParseError};
 
-pub fn parse_file(path: &str) -> std::io::Result<()> {
+pub fn parse_file(path: &str) -> Result<(), Err> {
     let s = fs::read_to_string(path)?;
     match violet::FileParser::new().parse(s.as_str()) {
         Ok(_) => {
             println!("parse ok");
+            Ok(())
         }
-        Err(e) => {
-            println!("parse failed {:?}", e);
-        }
+        Err(e) => match e {
+            InvalidToken { location } => Err(ParseError {
+                src: NamedSource::new(path, s[location..location + 1].to_string()),
+                bad_bit: (location, location + 1).into(),
+                reason: "InvalidToken".to_string(),
+            })?,
+            UnrecognizedEOF { location, expected } => Err(ParseError {
+                src: NamedSource::new(path, s[location - 1..location].to_string()),
+                bad_bit: (location - 1, location).into(),
+                reason: "UnrecognizedEOF".to_string(),
+            })?,
+            UnrecognizedToken { token, expected } => Err(ParseError {
+                src: NamedSource::new(path, s[token.0..token.2].to_string()),
+                bad_bit: (token.0, token.2 - token.0).into(),
+                reason: "UnrecognizedToken".to_string(),
+            })?,
+            ExtraToken { token } => Err(ParseError {
+                src: NamedSource::new(path, s[token.0..token.2].to_string()),
+                bad_bit: (token.0, token.2 - token.0).into(),
+                reason: "extra token".to_string(),
+            })?,
+            User { error } => Err(ParseError {
+                src: NamedSource::new(path, ""),
+                bad_bit: (0, 1).into(),
+                reason: error.to_string(),
+            })?,
+        },
     }
-    Ok(())
 }
 
 #[test]
 fn module_simple_case() {
     assert!(violet::ModParser::new().parse("(module hello)").is_ok());
-}
-
-#[test]
-fn test_output() {
-    println!("{:?}", violet::ModParser::new().parse("(modul hello)"));
 }
