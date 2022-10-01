d6228b35ef37da7938f8b689459c18865e792a15
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2022-10-01 08:29:50 +0800

    pick correct parser

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -1,10 +1,11 @@
 use std::fs;
 
+pub mod error;
 pub mod violet;
 
 pub fn parse_file(path: &str) -> std::io::Result<()> {
     let s = fs::read_to_string(path)?;
-    match violet::TermParser::new().parse(s.as_str()) {
+    match violet::FileParser::new().parse(s.as_str()) {
         Ok(_) => {
             println!("parse ok");
         }
@@ -18,4 +19,9 @@ pub fn parse_file(path: &str) -> std::io::Result<()> {
 #[test]
 fn module_simple_case() {
     assert!(violet::ModParser::new().parse("(module hello)").is_ok());
+}
+
+#[test]
+fn test_output() {
+    println!("{:?}", violet::ModParser::new().parse("(modul hello)"));
 }
