732964b0ce1eb0d9585e1d38efac0466b11ba7a6
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2022-10-08 05:23:39 +0800

    [dep] reporter: pretty diagnostic library for compiler
    Signed-off-by: Lîm Tsú-thuàn <dannypsnl@gmail.com>

--- info.rkt
+++ info.rkt
@@ -1,6 +1,6 @@
 #lang info
 (define collection "violet")
-(define deps '("base"))
+(define deps '("base" "reporter"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define pkg-desc "")
 (define version "0.0")
