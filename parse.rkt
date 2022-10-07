37342d2df0f0b88ba02a3f75637635a2cdbbb110
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2022-10-08 05:47:18 +0800

    parse top level definitions
    TODO: parse type

--- parse.rkt
+++ parse.rkt
@@ -14,7 +14,23 @@
             #:error-code "E0001"
             #:message "invalid module syntax"
             #:target mod
-            #:labels (list (label mod "here"
-                                  #:color (color:red)))
-            #:hint "help: read the formal description about the module syntax"))])
+            #:labels (list (label mod "here" #:color (color:red)))
+            #:hint "help: read the formal description of the module syntax"))])
+
+  (for ([top top-list])
+    (syntax-parse top
+      #:datum-literals (: define)
+      [(: name:id : ty)
+       'ty]
+      [(define name:id exp)
+       'def-var]
+      [(define (name:id p*:id ...) exp)
+       'def-proc]
+      [else (raise
+             (report
+              #:error-code "E0002"
+              #:message "invalid top level syntax"
+              #:target top
+              #:labels (list (label top "here" #:color (color:red)))
+              #:hint "help: read the formal description of the top level syntax"))]))
   )
