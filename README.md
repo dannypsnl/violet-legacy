780ce4850a4193cf3c949740b2ff64d21495482d
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2022-10-10 05:56:40 +0800

    [dep] racket-llvm
    update install command example in README
    
    Signed-off-by: Lîm Tsú-thuàn <dannypsnl@gmail.com>

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ A scheme-like language focuses on
 You will need to install LLVM, via any package manager would be fine, here is an example for macOS.
 
 ```shell
-brew install llvm@14
+brew install llvm
 ```
 
 Git hooks
--- info.rkt
+++ info.rkt
@@ -1,6 +1,8 @@
 #lang info
 (define collection "violet")
-(define deps '("base" "reporter"))
+(define deps '("base"
+               "reporter"
+               "racket-llvm"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define pkg-desc "")
 (define version "0.0")
