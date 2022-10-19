23db595225facf7ca8832753cb3b72d6b03f1035
kwguo <shana0440@gmail.com>
2022-10-19 19:23:01 +0800

    use main module to invoke command module

--- command.rkt
+++ command.rkt
@@ -1,27 +1,29 @@
 #lang racket
+(provide run-command)
+
 (require racket/cmdline)
 (require "build.rkt"
          "compile.rkt")
 
-(define command
-  (command-line
-    #:program "violet"
-    #:args (subcommand . rest)
-    (match subcommand
-    ["build"
-      (command-line
-        #:argv rest
-        #:args (dir-path)
-        (cons "build" dir-path)
-      )]
-    ["compile"
-      (command-line
-        #:argv rest
-        #:args (file-path)
-        (cons "compile" file-path))])))
-
-(match command
-[(cons "build" dir-path)
-  (build dir-path)]
-[(cons "compile" file-path)
-  (compile-all file-path)])
+(define (run-command)
+  (define command
+    (command-line
+      #:program "violet"
+      #:args (subcommand . rest)
+      (match subcommand
+      ["build"
+        (command-line
+          #:argv rest
+          #:args (dir-path)
+          (cons "build" dir-path)
+        )]
+      ["compile"
+        (command-line
+          #:argv rest
+          #:args (file-path)
+          (cons "compile" file-path))])))
+  (match command
+  [(cons "build" dir-path)
+    (build dir-path)]
+  [(cons "compile" file-path)
+    (compile-all file-path)]))
--- main.rkt
+++ main.rkt
@@ -1,42 +1,7 @@
 #lang racket
-(require racket/runtime-path
-         racket-llvm
-         reporter)
-(require "parse.rkt"
+(require "command.rkt"
          "tyck.rkt"
          "codegen.rkt")
 
-(define-runtime-path test-example "example/hello.ss")
-
-(define (collect in)
-  (define sexp-list '())
-  (let loop ([r-sexp (read-syntax (object-name in) in)])
-    (cond
-      [(eof-object? r-sexp) (void)]
-      [else (set! sexp-list (cons r-sexp sexp-list))
-            (loop (read-syntax (object-name in) in))]))
-  (reverse sexp-list))
-
-(define (compile-all path)
-  (define in (open-input-file path))
-  (port-count-lines! in)
-  (define sexp-list (collect in))
-  (with-handlers ([Report? displayln])
-    ((compose (lambda (llvm-file-path)
-                (system* (find-executable-path "llc") llvm-file-path "-filetype=obj"))
-              (lambda (mod)
-                (define llvm-file-path (path-replace-extension path #".bc"))
-                (llvm-write-bitcode-to-file mod llvm-file-path)
-                llvm-file-path)
-              (lambda (mod)
-                (display (llvm-module->string mod))
-                mod)
-              codegen-mod
-              type-check-module
-              parse-mod-file)
-     sexp-list)))
-
 (module+ main
-  ; TODO: multiple modules, parallel compilation
-  (compile-all test-example)
-  )
+  (run-command))
