b6d1a46f30039f38723f9ee6ed7bb27f264d6b0f
kwguo <shana0440@gmail.com>
2022-11-07 02:22:10 +0800

    compile file in future

--- command/build.rkt
+++ command/build.rkt
@@ -3,12 +3,10 @@
 (require "compile.rkt")
 
 (define (build dir-path
-  #:debug-llvm? [debug-llvm? #f])
-  ; FIXME: no need to build up a list, just directly compile them
-  (define files
-    (fold-files
-     (lambda (path _ acc)
-       (when [string-suffix? (path->string path) ".ss"]
-        (compile-to-obj/exe path #:debug-llvm? debug-llvm?)))
-     '() dir-path))
-  (println files))
+               #:debug-llvm? [debug-llvm? #f])
+  (find-files
+    (lambda (path)
+      (when [string-suffix? (path->string path) ".ss"]
+        (touch (future (lambda ()
+          (compile-to-obj/exe path #:debug-llvm? debug-llvm?))))))
+    dir-path))
