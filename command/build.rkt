bad875886aef719352ee24c4c8f17ec8b498539a
kwguo <shana0440@gmail.com>
2022-11-27 17:45:46 +0800

    compile files asynchronously

--- command/build.rkt
+++ command/build.rkt
@@ -1,11 +1,23 @@
 #lang racket
 (provide build)
+(require racket/async-channel)
 (require "compile.rkt")
 
 (define (build dir-path)
-  (find-files
-    (lambda (path)
-      (when [string-suffix? (path->string path) ".ss"]
-        (touch (future (lambda ()
-          (compile-to-obj/exe path #:debug-llvm? #f))))))
+  (define paths (find-files
+    (lambda (path) (path-has-extension? path #".ss"))
     dir-path))
+
+  (define ch (make-async-channel))
+  (define task-count (make-fsemaphore (length paths)))
+  (define proc-count (make-fsemaphore (processor-count)))
+  (for/async ([path paths])
+    (fsemaphore-wait proc-count)
+    (compile-to-obj/exe path #:debug-llvm? #f)
+    (fsemaphore-wait task-count)
+    (async-channel-put ch (fsemaphore-count task-count))
+    (fsemaphore-post proc-count))
+  (let loop ()
+    (if (= 0 (async-channel-get ch))
+      (void)
+      (loop))))
