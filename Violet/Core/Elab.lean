ab4597edb313fe53a6ca3f6ed438cde16ee3c6d3
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2023-05-20 22:12:37 +0800

    draft infer
    Signed-off-by: Lîm Tsú-thuàn <dannypsnl@gmail.com>

--- Violet/Core/Elab.lean
+++ Violet/Core/Elab.lean
@@ -1,7 +1,8 @@
 import Violet.Core.Eval
+import Violet.Ast.Surface
 
 namespace Violet.Core
-open Violet.Ast.Core
+open Violet.Ast
 
 @[reducible]
 abbrev TypCtx := List (String × VTy)
@@ -11,5 +12,20 @@ structure ElabContext where
   lvl : Lvl
   typCtx : TypCtx
   mctx : MetaCtx
+
+def infer [Monad m] [MonadState MetaCtx m] [MonadExcept String m]
+  (ctx : ElabContext) (tm : Surface.Tm) : m (Core.Tm × VTy) := do
+  match tm with
+  | .var x =>
+    match ctx.typCtx.lookup x with
+    | .some a => return (.var x, a)
+    | .none => throw s!"no variable named `{x}`"
+  -- TODO: a good idea would be having two lambda forms
+  -- 1. lam x => t
+  -- 2. lam (x : T) => t
+  --
+  -- The first one cannot be inferred, but the second one can.
+  | .lam x t => throw "cannot infer lambda without type annotation"
+  | _ => sorry
 
 end Violet.Core
