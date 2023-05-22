aedcaa3d123dbc383c0d31d3fbbd5646182a02b6
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2023-05-22 22:09:16 +0800

    bind data type into context
    Signed-off-by: Lîm Tsú-thuàn <dannypsnl@gmail.com>

--- Violet.lean
+++ Violet.lean
@@ -3,9 +3,20 @@ import Violet.Parser
 import Violet.Core.Elaboration
 
 namespace Violet.Core
+open Violet.Ast
 open Violet.Ast.Surface (Program)
 
 abbrev ElabM := StateT MetaCtx (ExceptT String IO)
+
+def defineM
+  (name : String) (ty : Surface.Typ) (val : Surface.Tm)
+  : StateT ElabContext (StateT MetaCtx (ExceptT String IO)) Unit := do
+  let ctx ← get
+  let ty ← ctx.check ty Val.type (m := ElabM)
+  let ty ← ctx.env.eval ty (m := ElabM)
+  let val ← ctx.check val ty (m := ElabM)
+  let val ← ctx.env.eval val (m := ElabM)
+  set <| ctx.define name val ty
 
 def innerCheck (p : Program)
   : StateT ElabContext (StateT MetaCtx (ExceptT String IO)) Unit := do
@@ -13,20 +24,21 @@ def innerCheck (p : Program)
     IO.println s!"checking {d}"
     match d with
     | .def name tele ret_ty body =>
-      let ctx ← get
-
-      let a := tele.foldr (λ (x, mode, a) b => .pi mode x a b) ret_ty
-      let a ← ctx.check a Val.type (m := ElabM)
-      let a ← ctx.env.eval a (m := ElabM)
-
-      let t := tele.foldr (λ (x, _, _) body => .lam x body) body
-      let t ← ctx.check t a (m := ElabM)
-      let t ← ctx.env.eval t (m := ElabM)
-
-      set <| ctx.define name a t
-    | .data name constructors =>
-      IO.println "skip data"
-  return ()
+      let ty : Surface.Typ := tele.foldr (λ (x, mode, a) b => .pi mode x a b) ret_ty
+      let val := tele.foldr (λ (x, _, _) body => .lam x body) body
+      defineM name ty val
+    | .data dataName constructors =>
+      -- the type name is the value of that type directly
+      defineM dataName .type dataName
+      for (name, tys) in constructors do
+        let ty := tys.foldr (λ ty b => .pi .explicit "_" ty b) (.var dataName)
+        let mut fresh := "_"
+        let mut val := .var name
+        -- no need to depend on type, so we can ignore the order problem here
+        for _ in tys do
+          val := .lam fresh (.app val fresh)
+          fresh := fresh ++ "'"
+        defineM name ty val
 
 end Violet.Core
 namespace Violet.Ast.Surface
