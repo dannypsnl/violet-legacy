d102911fd3e015bf2a6a7f294ea4cbc1f016bbeb
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2023-06-02 21:25:15 +0800

    regroup the test
    Signed-off-by: Lîm Tsú-thuàn <dannypsnl@gmail.com>

--- Tests/Parsing.lean
+++ Tests/Parsing.lean
@@ -13,26 +13,28 @@ instance [BEq α] : BEq (Except ε α) where
   beq | .ok x, .ok y => x == y
       | _, _ => false
 
-def main := lspecIO $
-  test "identifier" (term.run "a" == .ok "a")
-  $ group "pi type" $
-    test "explicit"
-      (term.run "(a : Type) → Type" == .ok (.pi .explicit "a" .type .type))
-    $ test "implicit"
-      (term.run "{a : Type} → Nat" == .ok (.pi .implicit "a" .type (.var "Nat")))
-    $ test "non dependent"
-      (term.run "a → b" == .ok (.pi .explicit "_" (.var "a") (.var "b")))
-    $ test "test non-unicode"
-      (term.run "(a : Type) -> Type" |> Except.isOk)
-  $ group "application" $
-    test "$ operator"
-      (term.run "a $ b c" == .ok (.app .explicit (.var "a") (.app .explicit (.var "b") (.var "c"))))
-    $ test "<| operator"
-      (term.run "a <| b <| c" == .ok (.app .explicit (.var "a")
-        (.app .explicit (.var "b") (.var "c"))))
-    $ test "|> operator"
-      (term.run "d |> b c |> a" == .ok (
-        .app .explicit (.var "a")
-          (.app .explicit
-            (.app .explicit (.var "b") (.var "c"))
-            (.var "d"))))
+def main : IO UInt32 :=
+  return [
+    ← lspecIO $ test "identifier" (term.run "a" == .ok "a"),
+    ← lspecIO $ group "pi type" $
+      test "explicit"
+        (term.run "(a : Type) → Type" == .ok (.pi .explicit "a" .type .type))
+      $ test "implicit"
+        (term.run "{a : Type} → Nat" == .ok (.pi .implicit "a" .type (.var "Nat")))
+      $ test "non dependent"
+        (term.run "a → b" == .ok (.pi .explicit "_" (.var "a") (.var "b")))
+      $ test "test non-unicode"
+        (term.run "(a : Type) -> Type" |> Except.isOk),
+    ← lspecIO $ group "application" $
+      test "$ operator"
+        (term.run "a $ b c" == .ok (.app .explicit (.var "a") (.app .explicit (.var "b") (.var "c"))))
+      $ test "<| operator"
+        (term.run "a <| b <| c" == .ok (.app .explicit (.var "a")
+          (.app .explicit (.var "b") (.var "c"))))
+      $ test "|> operator"
+        (term.run "d |> b c |> a" == .ok (
+          .app .explicit (.var "a")
+            (.app .explicit
+              (.app .explicit (.var "b") (.var "c"))
+              (.var "d"))))
+  ].foldl (λ acc x => acc + x) 0
